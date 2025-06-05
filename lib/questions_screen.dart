8e67c56fd01e84437a44cd36fc125ed23a9f63c5
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 12:25:22 +0200

    change eleveted button to new widget custom answer button

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/answer_button.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -17,9 +18,9 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
         children: [
           const Text('Bla bla bla'),
           const SizedBox(height: 30,),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
-          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+          AnswerButton(answerText: 'Answer 1', onTap: (){}),
+          AnswerButton(answerText: 'Answer 2', onTap: (){}),
+          AnswerButton(answerText: 'Answer 3', onTap: (){}),
         ],
       ),
     );
