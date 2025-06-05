4822aa2b78b45bc5b40062d4457bdf3abd9ec423
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 15:56:13 +0200

    replace static data with constant of date coded

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -15,7 +15,10 @@ class AnswerButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onTap,
       style: ElevatedButton.styleFrom(
-        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 40,
+        ),
         backgroundColor: const Color.fromARGB(
           255,
           192,
