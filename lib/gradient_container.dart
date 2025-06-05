e1d7d89acb05a98dcae9128d74ea4202c70d3177
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 10:27:25 +0200

    create first layout and new start_screen widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -4,10 +4,13 @@ const Alignment startLinearGradient = Alignment.topLeft;
 const Alignment endLinearGradient = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key, required this.gradientColors});
+  const GradientContainer({
+    super.key,
+    required this.gradientColors,
+  });
 
   final List<Color> gradientColors;
-  
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -15,7 +18,35 @@ class GradientContainer extends StatelessWidget {
         gradient: LinearGradient(
           colors: gradientColors,
           begin: startLinearGradient,
-          end: endLinearGradient
+          end: endLinearGradient,
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+            SizedBox(height: 80),
+            Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 80),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                foregroundColor: Colors.white,
+                side: BorderSide.none
+              ),
+              onPressed: () {},
+              child: const Text('Start Quiz'),
+            ),
+          ],
         ),
       ),
     );
