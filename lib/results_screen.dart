dd3cdd443e5f13c57b29ad20868c52a07139eb26
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 20:00:22 +0200

    create method getSummaryData

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.choosenAnswer});
 
   final List<String> choosenAnswer;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for(var i = 0; i < choosenAnswer.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i],
+          'correct_answer': questions[i].answers[0],
+          'user_answer': choosenAnswer[i]
+        }
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
