d90f36f064bdc3504002cae66f0b799bbe9c4169
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 16:14:49 +0200

    create a new method to model to return a shuffle list of answers

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -30,7 +30,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30,),
-            ...currentQuestion.answers.map((answer) {
+            ...currentQuestion.getShuffleAnswers().map((answer) {
               return AnswerButton(answerText: answer, onTap: () {});
             }),
           ],
