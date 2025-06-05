4af2f0eb321c53661e9b5dc5ccd6bc83ac53cfe7
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 11:46:39 +0200

    add center to column and initState in Questions screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -10,7 +10,13 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget activeScreen = const StartScreen(switchScreen);
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(switchScreen);
+  }
 
   void switchScreen() {
     setState(() {
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -7,35 +7,37 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-          color: const Color.fromARGB(150, 255, 255, 255), //trick to put opacity
-        ),
-        const SizedBox(height: 80),
-        const Text(
-          'Learn Flutter the fun way!',
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.white,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255), //trick to put opacity
           ),
-        ),
-        const SizedBox(height: 80),
-        OutlinedButton.icon(
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,
-            side: BorderSide(
-              color: Colors.deepOrange
-            )
+          const SizedBox(height: 80),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 25,
+              color: Colors.white,
+            ),
           ),
-          onPressed: startQuiz,
-          icon: const Icon(Icons.arrow_right_alt),
-          label: const Text('Start Quiz'),
-        ),
-      ],
+          const SizedBox(height: 80),
+          OutlinedButton.icon(
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              side: BorderSide(
+                color: Colors.deepOrange
+              )
+            ),
+            onPressed: startQuiz,
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
     );
   }
 }
