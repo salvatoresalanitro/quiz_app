b497e52056dec801f92b1e1d1fadfa7ea47631f4
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 12:23:02 +0200

    add parameters to answer button new class

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 
 class AnswerButton extends StatelessWidget {
-  const AnswerButton(this.answerText, {super.key});
+  const AnswerButton({
+    super.key,
+    required this.answerText,
+    required this.onTap,
+  });
 
   final String answerText;
+  final void Function() onTap;
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-
-      ),
-      child: const Text('Answer 1'),
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(),
+      child: Text(answerText),
     );
   }
 }
