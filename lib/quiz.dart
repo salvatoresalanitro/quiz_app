fa748e5b614bcc5aa3c8b4d3f15835571b3b77f3
Salvo <salvatoresalanitro94@gmail.com>
2025-06-05 17:58:32 +0200

    add switch statement to change in the new screen

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/data/questions.dart';
 import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/results_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -26,16 +27,20 @@ class _QuizState extends State<Quiz> {
     if(selectedAnswers.length == questions.length) {
       setState(() {
         selectedAnswers = [];
-        activeScreen = 'start-screen';
+        activeScreen = 'results-screen';
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    final screenWidget = activeScreen == 'start-screen'
-      ? StartScreen(switchScreen)
-      : QuestionsScreen(onSelectAnswer: chooseAnswer,);
+    
+    final screenWidget = switch (activeScreen) {
+      'start-screen' => StartScreen(switchScreen),
+      'questions-screen' => QuestionsScreen(onSelectAnswer: chooseAnswer),
+      _ => ResultsScreen(),
+    };
+
 
     return MaterialApp(
       home: Scaffold(
